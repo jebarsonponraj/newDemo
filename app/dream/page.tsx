9b87fe6ad7bc7f0bb193ac8c439acfb21c0470da
jebarsonponraj <jebarsonponraj@gmail.com>
2024-04-22 11:22:25 +0530

    Removed store

--- app/dream/page.tsx
+++ app/dream/page.tsx
@@ -100,11 +100,11 @@ export default function DreamPage() {
     return (
         <>
         <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
-            <Link href="/store">
+            {/* <Link href="/store">
                 <button className="z-50 absolute top-0 right-0 m-4 bg-blue-500 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-600">
                     Generated Images
                 </button>
-            </Link>
+            </Link> */}
             <main className="z-50 flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4 sm:mb-0 mb-8">
                 <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-slate-100 sm:text-6xl mb-5">
                     Generate your <span className="text-blue-600">dream</span>{" "}
